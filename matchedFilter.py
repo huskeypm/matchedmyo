2aee920be04f2601915de1647e560c05a8bed32a
Dfco222 <dfco222@g.uky.edu>
2018-11-06 11:43:44 -0500

    Began writing routines for 3D analysis. Wrote routines to read in and save 3D arrays. Wrote routine to rotate 3D filter.

--- matchedFilter.py
+++ matchedFilter.py
@@ -4,6 +4,7 @@ Performs basic matched filter test by FFT-correlating a measured signal with an 
 import numpy as np
 # import modules we'll need 
 import scipy.fftpack as fftp
+from scipy import ndimage
 import cv2
 
 
@@ -16,52 +17,59 @@ def matchedFilter(
   parsevals=False,
   demean=True
   ):
-  useCV2 = True
-  if useCV2:
-    ### TRYING OUT NEW CV2 CONVOLUTION CODE
-    h = cv2.filter2D(dimg,-1,daFilter)
-    return h
+
+  ### Check to see if the image is 2D or 3D. cv2 algorithm is a bit quicker for 2D but it is not compatible with 3D images
+  if len(np.shape(dimg)) == 3:
+    ## Use scipy ndimage routine to convolve the 3D image and Filter
+    h = ndimage.convolve(dimg,daFilter,mode='mirror')
   else:
-    # placeholder for 'noise' component (will refine later)
-    fsC = np.ones(np.shape(dimg))
-  
-    ## prepare img
-    # demean/shift img
-    if demean:
-      sdimg = fftp.fftshift(dimg - np.mean(dimg))
+    useCV2 = True
+    if useCV2:
+      ### TRYING OUT NEW CV2 CONVOLUTION CODE
+      ###   This will eventually take over as default option since it is so much faster than in-house MF code
+      h = cv2.filter2D(dimg,-1,daFilter)
+      return h
     else:
-      sdimg = fftp.fftshift(dimg)
-    # take FFT 
-    fsdimg = fftp.fft2(sdimg)
+      # placeholder for 'noise' component (will refine later)
+      fsC = np.ones(np.shape(dimg))
 
-    ## zero-pad filter
-    si = np.shape(dimg)
-    sf = np.shape(daFilter)
-    # add zeros
-    zeropad = np.zeros(si)
-    zeropad[:sf[0],:sf[1]]=daFilter
-    # shift original ligand by its Nyquist
-    szeropad = np.roll(\
-      np.roll(zeropad,-sf[0]/2+si[0]/2,axis=0),-sf[1]/2+si[1]/2,axis=1)
-    f= szeropad
+      ## prepare img
+      # demean/shift img
+      if demean:
+        sdimg = fftp.fftshift(dimg - np.mean(dimg))
+      else:
+        sdimg = fftp.fftshift(dimg)
+      # take FFT 
+      fsdimg = fftp.fft2(sdimg)
 
-    ## signal
-    # shift,demean filter
-    if demean:
-      sfilter = fftp.fftshift(f- np.mean(f))
-    else:
-      sfilter = fftp.fftshift(f)
-    # take FFT
-    fsfilter= fftp.fft2(sfilter)
+        ## zero-pad filter
+      si = np.shape(dimg)
+      sf = np.shape(daFilter)
+      # add zeros
+      zeropad = np.zeros(si)
+      zeropad[:sf[0],:sf[1]]=daFilter
+      # shift original ligand by its Nyquist
+      szeropad = np.roll(\
+        np.roll(zeropad,-sf[0]/2+si[0]/2,axis=0),-sf[1]/2+si[1]/2,axis=1)
+      f= szeropad
 
-    ## matched filter
-    fsh = fsdimg * fsfilter / fsC
-    #fsh = np.real( fsh ) 
-    sh = fftp.ifft2(fsh)
-    h = fftp.ifftshift(sh)
-    h = np.real(h)
+      ## signal
+      # shift,demean filter
+      if demean:
+        sfilter = fftp.fftshift(f- np.mean(f))
+      else:
+        sfilter = fftp.fftshift(f)
+      # take FFT
+      fsfilter= fftp.fft2(sfilter)
 
-    ## apply parsevals
-    if parsevals:
-      h *= 1/np.float(np.prod(np.shape(h)))
-    return h 
+      ## matched filter
+      fsh = fsdimg * fsfilter / fsC
+      #fsh = np.real( fsh ) 
+      sh = fftp.ifft2(fsh)
+      h = fftp.ifftshift(sh)
+      h = np.real(h)
+
+      ## apply parsevals
+      if parsevals:
+        h *= 1/np.float(np.prod(np.shape(h)))
+      return h 
