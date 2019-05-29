0a1a71e39a426bc0c1649494f1c844666d0c4e7a
Dfco222 <dfco222@g.uky.edu>
2019-05-29 11:04:07 -0400

    Added some fixes to the python3 installation bash file

--- installation_python3.bash
+++ installation_python3.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pip3 install --user opencv-python==3.4.1.15
+pip3 install --user opencv-python==4.1.0.25
 
 pip3 install --user imutils==0.4.6
 
-pip3 install --user numpy==1.15.4
+pip3 install --user numpy==1.16.4
 
-pip3 install --user matplotlib==2.2.3
+pip3 install --user matplotlib==3.1.0
 ### The following is necessary to use matplotlib in it's current implementation
 sudo apt-get install python3-tk
 
