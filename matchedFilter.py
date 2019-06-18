0a8abd8368fb24d257a9948dd3fc25c9654df497
ssbhatt <shashanksbhatt@gmail.com>
2019-06-18 19:40:05 +0000

    Added support for using gputools to speed up convolutions

--- matchedFilter.py
+++ matchedFilter.py
@@ -8,6 +8,10 @@ from scipy import ndimage
 import cv2
 from six import iteritems
 
+
+## Shashank edit
+from gputools.convolve.convolve import _convolve_buf
+
 ##
 ## Basic steps of matched filtering process
 ## 
@@ -15,74 +19,85 @@ def matchedFilter(
   dimg, # imgs wherein signal (filter) is hypothesized to exist
   daFilter,# signal 
   parsevals=False,
-  demean=True
+  demean=True,
+  useGPU=False ## Shashank edit
   ):
-
-  ### Check to see if the image is 2D or 3D. cv2 algorithm is a bit quicker for 2D but it is not compatible with 3D images
-  if len(np.shape(dimg)) == 3:
+  
+  if useGPU:
+    ## Shashank edit
     if isinstance(daFilter, list):
-      # this indicates that we've decomposed the filter for sequential filtering
-      h = sequential_filtering(dimg, daFilter)
-    
+        h = sequential_filtering(dimg, daFilter, useGPU)
     else:
-      # Use scipy ndimage routine to convolve the 3D image and Filter
-      h = ndimage.convolve(dimg,daFilter)#,mode='mirror')
+        h = _convolve_buf(dimg, daFilter)
+  
+    return h.get()
+  
   else:
-    useCV2 = True
-    if useCV2:
+    ### Check to see if the image is 2D or 3D. cv2 algorithm is a bit quicker for 2D but it is not compatible with 3D images
+    if len(np.shape(dimg)) == 3:
       if isinstance(daFilter, list):
         # this indicates that we've decomposed the filter for sequential filtering
         h = sequential_filtering(dimg, daFilter)
-
+      
       else:
-        h = cv2.filter2D(dimg,-1,daFilter)
-
+        # Use scipy ndimage routine to convolve the 3D image and Filter
+        h = ndimage.convolve(dimg,daFilter)#,mode='mirror')
     else:
-      # placeholder for 'noise' component (will refine later)
-      fsC = np.ones(np.shape(dimg))
+      useCV2 = True
+      if useCV2:
+        if isinstance(daFilter, list):
+          # this indicates that we've decomposed the filter for sequential filtering
+          h = sequential_filtering(dimg, daFilter)
 
-      ## prepare img
-      # demean/shift img
-      if demean:
-        sdimg = fftp.fftshift(dimg - np.mean(dimg))
+        else:
+          h = cv2.filter2D(dimg,-1,daFilter)
+
       else:
-        sdimg = fftp.fftshift(dimg)
-      # take FFT 
-      fsdimg = fftp.fft2(sdimg)
+        # placeholder for 'noise' component (will refine later)
+        fsC = np.ones(np.shape(dimg))
 
-        ## zero-pad filter
-      si = np.shape(dimg)
-      sf = np.shape(daFilter)
-      # add zeros
-      zeropad = np.zeros(si)
-      zeropad[:sf[0],:sf[1]]=daFilter
-      # shift original ligand by its Nyquist
-      szeropad = np.roll(\
-        np.roll(zeropad,-sf[0]/2+si[0]/2,axis=0),-sf[1]/2+si[1]/2,axis=1)
-      f= szeropad
+        ## prepare img
+        # demean/shift img
+        if demean:
+          sdimg = fftp.fftshift(dimg - np.mean(dimg))
+        else:
+          sdimg = fftp.fftshift(dimg)
+        # take FFT 
+        fsdimg = fftp.fft2(sdimg)
 
-      ## signal
-      # shift,demean filter
-      if demean:
-        sfilter = fftp.fftshift(f- np.mean(f))
-      else:
-        sfilter = fftp.fftshift(f)
-      # take FFT
-      fsfilter= fftp.fft2(sfilter)
+          ## zero-pad filter
+        si = np.shape(dimg)
+        sf = np.shape(daFilter)
+        # add zeros
+        zeropad = np.zeros(si)
+        zeropad[:sf[0],:sf[1]]=daFilter
+        # shift original ligand by its Nyquist
+        szeropad = np.roll(\
+          np.roll(zeropad,-sf[0]/2+si[0]/2,axis=0),-sf[1]/2+si[1]/2,axis=1)
+        f= szeropad
 
-      ## matched filter
-      fsh = fsdimg * fsfilter / fsC
-      #fsh = np.real( fsh ) 
-      sh = fftp.ifft2(fsh)
-      h = fftp.ifftshift(sh)
-      h = np.real(h)
+        ## signal
+        # shift,demean filter
+        if demean:
+          sfilter = fftp.fftshift(f- np.mean(f))
+        else:
+          sfilter = fftp.fftshift(f)
+        # take FFT
+        fsfilter= fftp.fft2(sfilter)
 
-      ## apply parsevals
-      if parsevals:
-        h *= 1/np.float(np.prod(np.shape(h)))
-  return h 
+        ## matched filter
+        fsh = fsdimg * fsfilter / fsC
+        #fsh = np.real( fsh ) 
+        sh = fftp.ifft2(fsh)
+        h = fftp.ifftshift(sh)
+        h = np.real(h)
 
-def sequential_filtering(dimg,daFilter_list):
+        ## apply parsevals
+        if parsevals:
+          h *= 1/np.float(np.prod(np.shape(h)))
+    return h
+
+def sequential_filtering(dimg,daFilter_list,useGPU=False):
   '''Function to perform sequential filtering of 'dimg' given a list of filters.
   This is advantageous since there is a massive speedup for large 2D or 3D filters that can be 
   linearly decomposed.'''
@@ -95,7 +110,12 @@ def sequential_filtering(dimg,daFilter_list):
     
   #   elif isinstance(axis, str):
   #     dimg = ndimage.convolve(dimg, filt)
-  for filt in daFilter_list:
-    dimg = ndimage.convolve(dimg, filt)
-    
+  
+  if useGPU:
+      for filt in daFilter_list:
+          dimg = _convolve_buf(dimg, filt)
+  else:
+      for filt in daFilter_list:
+          dimg = ndimage.convolve(dimg, filt)
+
   return dimg
